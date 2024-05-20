92a60e1b72f1f71454f4bc181964bef8df17f253
Mehmet Baran Özdeniz <Ozdenizm6@gmail.com>
2024-05-20 17:36:11 +0300

    SpringSecurity bağımlılığı eklendi ve swagger'a auth eklemesi yapıldı

--- src/main/java/com/auth/user/api/UserApi.java
+++ src/main/java/com/auth/user/api/UserApi.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
@@ -23,7 +24,7 @@ import java.util.UUID;
 @Validated
 public interface UserApi {
 
-    @Operation(operationId = "signUpUser", summary = "Sign up user.")
+    @Operation(operationId = "signUpUser", summary = "Sign up user.", security = @SecurityRequirement(name = "basicAuth"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "SUCCESS", content = @Content(schema = @Schema(implementation = Boolean.class))),
             @ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(implementation = UserCreateDto.class))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class))),
@@ -37,7 +38,7 @@ public interface UserApi {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = {MediaType.APPLICATION_JSON_VALUE})
     ResponseEntity<UUID> signUp(@RequestBody @Valid UserCreateDto userCreateDto);
 
-    @Operation(operationId = "loginUser", summary = "Login user.")
+    @Operation(operationId = "loginUser", summary = "Login user.", security = @SecurityRequirement(name = "basicAuth"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "SUCCESS", content = @Content(schema = @Schema(implementation = UserDto.class))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class))),
             @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(implementation = Error.class))),
@@ -50,7 +51,7 @@ public interface UserApi {
     @GetMapping(value = "/get", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<UserDto> login(@RequestParam String email, @RequestHeader(value = "password") String password);
 
-    @Operation(operationId = "updateUser", summary = "Update user.")
+    @Operation(operationId = "updateUser", summary = "Update user.", security = @SecurityRequirement(name = "basicAuth"))
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "SUCCESS", content = @Content(schema = @Schema(implementation = UserDto.class))),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class))),
             @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(implementation = Error.class))),
@@ -63,7 +64,7 @@ public interface UserApi {
     @PutMapping(value = "/update/{email}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<UserDto> updateUser(@PathVariable String email, @RequestBody @Valid UserUpdateDto userUpdateDto);
 
-    @Operation(operationId = "deleteUser", summary = "Delete user.")
+    @Operation(operationId = "deleteUser", summary = "Delete user.", security = @SecurityRequirement(name = "basicAuth"))
     @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "No Content"),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class))),
             @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(implementation = Error.class))),
---
+++ src/main/java/com/auth/user/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.auth.user.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
