d17f5f53f114ecc9cb5281dec9c4b3f673a6d7f4
Mehmet Baran Özdeniz <Ozdenizm6@gmail.com>
2024-05-21 16:40:10 +0300

    Swagger'daki authorize problemi çözüldü

--- src/main/java/com/auth/user/config/SecurityConfig.java
+++ src/main/java/com/auth/user/config/SecurityConfig.java
@@ -5,17 +5,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @RequiredArgsConstructor
 @Configuration
@@ -27,11 +28,8 @@ public class SecurityConfig {
     private static final String[] AUTH_WHITELIST = {
             "/v3/api-docs/**",
             "/swagger-ui/**",
-            //"/docs/swagger-ui/**",
-            "/docs/swagger-ui/index.html#/",
-            "/swagger-ui.html",
-            //"/api/auth/**",
-            //"/api/test/**",
+            "/docs/swagger-ui/**",
+            "/swagger-ui.html"
     };
 
     @Bean
@@ -44,11 +42,16 @@ public class SecurityConfig {
                     registry.requestMatchers("/api/v1/user/update", "/api/v1/user/delete").hasRole("USER");
                     registry.anyRequest().authenticated();
                 })
-                //.formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
-                //.formLogin(Customizer.withDefaults())
-                //.formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .exceptionHandling(exceptionHandlingConfigurer ->
+                        exceptionHandlingConfigurer.authenticationEntryPoint(unauthorizedEntryPoint())
+                )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint unauthorizedEntryPoint() {
+        return new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED);
     }
 
     @Bean
